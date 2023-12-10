29d1f2ca8b86818f3376cd7898c25db9a829d378
Benjamin Regan <35966588+Bregann@users.noreply.github.com>
2023-12-10 12:31:17 +0000

    homepage designed

--- financemanager.web/pages/index.tsx
+++ financemanager.web/pages/index.tsx
@@ -1,31 +1,32 @@
-import { Container, Grid } from '@mantine/core'
+import { Container, Grid, Select, Table } from '@mantine/core'
 import classes from '../styles/Home.module.css'
+import { IconTrash } from '@tabler/icons-react'
 
 export default function Home (): JSX.Element {
   return (
     <>
 
-      <Container size='xl'>
-        <Grid>
-          <Grid.Col span={{ base: 12, xl: 3, lg: 3, md: 6, sm: 6, xs: 6 }}>
+      <Container size='lg'>
+        <Grid gutter={0}>
+          <Grid.Col span="auto">
             <div className={classes.box}>
               <h3 className={classes.header}>Money In</h3>
               <p className={classes.amount}>£100</p>
             </div>
           </Grid.Col>
-          <Grid.Col span={{ base: 12, xl: 3, lg: 3, md: 6, sm: 6, xs: 6 }}>
+          <Grid.Col span="auto">
             <div className={classes.box}>
               <h3 className={classes.header}>Money Spent</h3>
               <p className={classes.amount}>£100</p>
             </div>
           </Grid.Col>
-          <Grid.Col span={{ base: 12, xl: 3, lg: 3, md: 6, sm: 6, xs: 6 }}>
+          <Grid.Col span="auto">
             <div className={classes.box}>
               <h3 className={classes.header}>Money Left</h3>
               <p className={classes.amount}>£100</p>
             </div>
           </Grid.Col>
-          <Grid.Col span={{ base: 12, xl: 3, lg: 3, md: 6, sm: 6, xs: 6 }}>
+          <Grid.Col span="auto">
             <div className={classes.box}>
               <h3 className={classes.header}>Total in Savings</h3>
               <p className={classes.amount}>£100</p>
@@ -34,6 +35,71 @@ export default function Home (): JSX.Element {
         </Grid>
 
         <h1>Home</h1>
+
+        <div className={classes.tableBox}>
+          <Table striped>
+            <Table.Thead>
+              <Table.Tr>
+                <Table.Th>Icon</Table.Th>
+                <Table.Th>Merchant</Table.Th>
+                <Table.Th>Transaction Amount</Table.Th>
+                <Table.Th>Date</Table.Th>
+                <Table.Th>Pot Type</Table.Th>
+                <Table.Th></Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>
+              <Table.Tr>
+                <Table.Td>Ico</Table.Td>
+                <Table.Td>Sainsburys</Table.Td>
+                <Table.Td>£500</Table.Td>
+                <Table.Td>Today</Table.Td>
+                <Table.Td>
+                  <Select
+                    placeholder='Pick value'
+                    data={['Savings', 'None']}
+                    comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 } }}
+                  />
+                </Table.Td>
+                <Table.Td>
+                  <IconTrash className={classes.binIco}/>
+                </Table.Td>
+              </Table.Tr>
+              <Table.Tr>
+                <Table.Td>Ico</Table.Td>
+                <Table.Td>Sainsburys</Table.Td>
+                <Table.Td>£500</Table.Td>
+                <Table.Td>Today</Table.Td>
+                <Table.Td>
+                  <Select
+                    placeholder='Pick value'
+                    data={['Savings', 'None']}
+                    comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 } }}
+                  />
+                </Table.Td>
+                <Table.Td>
+                  <IconTrash className={classes.binIco}/>
+                </Table.Td>
+              </Table.Tr>
+              <Table.Tr>
+                <Table.Td>Ico</Table.Td>
+                <Table.Td>Sainsburys</Table.Td>
+                <Table.Td>£500</Table.Td>
+                <Table.Td>Today</Table.Td>
+                <Table.Td>
+                  <Select
+                    placeholder='Pick value'
+                    data={['Savings', 'None']}
+                    comboboxProps={{ transitionProps: { transition: 'pop', duration: 200 } }}
+                  />
+                </Table.Td>
+                <Table.Td>
+                  <IconTrash className={classes.binIco}/>
+                </Table.Td>
+              </Table.Tr>
+            </Table.Tbody>
+          </Table>
+        </div>
       </Container>
     </>
   )
